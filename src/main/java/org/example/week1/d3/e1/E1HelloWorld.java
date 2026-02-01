4ba6af43ef2bf39916700b26ff4af25ab9433b23
shivaregalla <shivarrac@gmail.com>
2026-02-01 22:39:39 +0530

    JavaHomework week1 day3

---
+++ src/main/java/org/example/week1/d3/e1/E1HelloWorld.java
@@ -0,0 +1,7 @@
+package org.example.week1.d3.e1;
+
+public class E1HelloWorld {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/week1/d3/e2/E2HelloWorld.java
@@ -0,0 +1,8 @@
+package org.example.week1.d3.e2;
+
+public class E2HelloWorld {
+    public static void main(String[] args) {
+
+
+    }
+}
